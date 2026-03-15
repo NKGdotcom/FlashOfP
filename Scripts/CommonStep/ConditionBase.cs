db6bd2cba1a635ce5c9e80084720d2ea8c798e0b
NKG <naka1028koki@gmail.com>
2026-03-15 22:17:03 +0900

    Delete Scripts/CommonStep/ConditionBase.cs

--- Scripts/CommonStep/ConditionBase.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public abstract class ConditionBase : MonoBehaviour
-{
-    public virtual void OnInitialize() { }
-
-    // ğŒ‚ğ–‚½‚µ‚½‚©‚Ç‚¤‚©‚ğ•Ô‚·
-    public abstract bool CheckCondition();
-}
