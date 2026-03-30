d77cbc178da2b65c0260770a28c520f565af8026
NKG <naka1028koki@gmail.com>
2026-03-31 00:27:31 +0900

    Add files via upload

--- Scripts/StageGimmick/PerfectConditions/Stage4Condition.cs
+++ Scripts/StageGimmick/PerfectConditions/Stage4Condition.cs
@@ -1,20 +1,24 @@
 using UnityEngine;
+using UnityEngine.UI;
 /// <summary>
 /// Stage 4 PerfectCondition
 /// </summary>
 public class Stage4Condition : BasePerfectCondition
 {
+    [SerializeField] private Image cameraImage;
     [SerializeField] private ResetStage resetStage;
     private int targetActiveNum = 0;
 
     public override bool IsPerfect()
     {
+        bool _isCameraActive = cameraImage.gameObject.activeSelf;
+        cameraImage.gameObject.SetActive(false);
         if (resetStage == null)
         {
             Debug.LogWarning("ResetStage がセットされていません！");
             return false;
         }
 
-        return resetStage.GetActiveExplosionCount() <= targetActiveNum;
+        return (resetStage.GetActiveExplosionCount() <= targetActiveNum) && _isCameraActive;
     }
 }
